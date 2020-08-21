70f701e07599becd01e25f30861e56cbb814deff
Darko Trifunovski <dtrifuno@gmail.com>
2020-08-21 22:47:21 +0000

    Solves Exercise 1.10

---
+++ src/main/java/core/java/se9/ch1/Ex10.java
@@ -0,0 +1,10 @@
+package core.java.se9.ch1;
+
+import java.util.Random;
+
+public class Ex10 {
+  public static void main(String[] args) {
+    Random gen = new Random();
+    System.out.println(Long.toString(Math.abs(gen.nextLong()), 36));
+  }
+}
