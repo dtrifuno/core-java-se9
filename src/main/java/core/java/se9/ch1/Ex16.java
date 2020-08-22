a4f272b3b5bec1335b4c9e54af54d31d4df2e650
Darko Trifunovski <dtrifuno@gmail.com>
2020-08-22 15:59:03 +0000

    Solves Exercise 1.16

---
+++ src/main/java/core/java/se9/ch1/Ex16.java
@@ -0,0 +1,20 @@
+package core.java.se9.ch1;
+
+public class Ex16 {
+  public static double average(double first, double... rest) {
+    double sum = first;
+    double c = 0;
+
+    // Kahan summation
+    for (double x : rest) {
+      double y = x - c;
+      double t = sum + y;
+      c = (t - sum) - y;
+      sum = t;
+
+    }
+
+    return sum / (rest.length + 1.0);
+  }
+  
+}
---
+++ src/test/java/core/java/se9/ch1/Ex16Test.java
@@ -0,0 +1,38 @@
+package core.java.se9.ch1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Ex16Test {
+  private static final double DELTA=0.0000000001;
+
+  @Test
+  void testAverageWithASingleEntry() {
+    assertEquals(-1429.743, Ex16.average(-1429.743), DELTA);
+  }
+
+  @Test
+  void testAverageWithTwoEntries() {
+    assertEquals(7.5, Ex16.average(7, 8), DELTA);
+  }
+
+  @Test
+  void testAverageWithThreeEntries() {
+    assertEquals(26.75, Ex16.average(90, 27, -100, 90), DELTA);
+  }
+
+  @Test
+  void testAverageWithConsecutiveIntegers() {
+    assertEquals(3.0, Ex16.average(1, 2, 3, 4, 5), DELTA);
+  }
+
+  @Test
+  void testAverageWithZeros() {
+    assertEquals(59 / 6.0, Ex16.average(0, 36, 0, -23, 10, 36), DELTA);
+  }
+
+  @Test
+  void testAverageWithDecimals() {
+    assertEquals(0.3702, Ex16.average(13.512, -12.651, -7.234, 5.874, 2.35), DELTA);
+  }
+}
