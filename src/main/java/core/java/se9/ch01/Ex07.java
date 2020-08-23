d1538c205f89c627775b840064ac2bc94dbd00b7
Darko Trifunovski <dtrifuno@gmail.com>
2020-08-23 02:40:05 +0000

    Solves Exercise 1.7

---
+++ src/main/java/core/java/se9/ch01/Ex07.java
@@ -0,0 +1,42 @@
+package core.java.se9.ch01;
+
+import java.util.Scanner;
+
+public class Ex07 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int x, y;
+    while (true) {
+      try {
+        System.out.print("Please enter the first integer: ");
+        x = Integer.parseUnsignedInt(in.next());
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid argument. Enter an integer between 0 and 4294967295.");
+        continue;
+      }
+      break;
+    }
+    while (true) {
+      try {
+        System.out.print("Please enter the second integer: ");
+        y = Integer.parseUnsignedInt(in.next());
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid argument. Enter an integer between 0 and 4294967295.");
+        continue;
+      }
+      break;
+    }
+
+    System.out.printf("%s + %s = %s\n", Integer.toUnsignedString(x), Integer.toUnsignedString(y),
+        Integer.toUnsignedString(x + y));
+    System.out.printf("%s - %s = %s\n", Integer.toUnsignedString(x), Integer.toUnsignedString(y),
+        Integer.toUnsignedString(x - y));
+    System.out.printf("%s * %s = %s\n", Integer.toUnsignedString(x), Integer.toUnsignedString(y),
+        Integer.toUnsignedString(x * y));
+    System.out.printf("%s / %s = %s\n", Integer.toUnsignedString(x), Integer.toUnsignedString(y),
+        Integer.toUnsignedString(Integer.divideUnsigned(x, y)));
+
+    in.close();
+  }
+}
