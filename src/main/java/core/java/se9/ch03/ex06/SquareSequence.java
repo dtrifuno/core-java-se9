bca2a8ed9efe75171184ec5787b19717517ae580
Darko Trifunovski <dtrifuno@gmail.com>
2020-09-09 19:15:31 +0000

    Solves Exercise 3.6

---
+++ src/main/java/core/java/se9/ch03/ex06/SquareSequence.java
@@ -0,0 +1,19 @@
+package core.java.se9.ch03.ex06;
+
+import java.math.BigInteger;
+
+public class SquareSequence implements Sequence<BigInteger> {
+  private BigInteger i = BigInteger.ZERO;
+
+  @Override
+  public boolean hasNext() {
+    return true;
+  }
+
+  @Override
+  public BigInteger next() {
+    i = i.add(BigInteger.ONE);
+    return i.multiply(i);
+  }
+  
+}
---
+++ src/test/java/core/java/se9/ch03/ex06/SquareSequenceTest.java
@@ -0,0 +1,29 @@
+package core.java.se9.ch03.ex06;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.Test;
+
+public class SquareSequenceTest {
+
+  @Test
+  void testSquareSequence() {
+    var seq = new SquareSequence();
+    assertTrue(seq.hasNext());
+    assertEquals(seq.next(), BigInteger.valueOf(1));
+    assertEquals(seq.next(), BigInteger.valueOf(4));
+    assertEquals(seq.next(), BigInteger.valueOf(9));
+    assertEquals(seq.next(), BigInteger.valueOf(16));
+    assertEquals(seq.next(), BigInteger.valueOf(25));
+    assertEquals(seq.next(), BigInteger.valueOf(36));
+    assertEquals(seq.next(), BigInteger.valueOf(49));
+    assertEquals(seq.next(), BigInteger.valueOf(64));
+    assertEquals(seq.next(), BigInteger.valueOf(81));
+    assertEquals(seq.next(), BigInteger.valueOf(100));
+    assertEquals(seq.next(), BigInteger.valueOf(121));
+    assertEquals(seq.next(), BigInteger.valueOf(144));
+    assertTrue(seq.hasNext());
+  }
+}
