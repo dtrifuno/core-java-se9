ca6f48b9fcc2d8b6167038c648326abd9c183750
Darko Trifunovski <dtrifuno@gmail.com>
2020-09-09 04:20:21 +0000

    Solves Exercise 8.10

---
+++ src/main/java/core/java/se9/ch08/Ex10.java
@@ -0,0 +1,27 @@
+package core.java.se9.ch08;
+
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
+
+import java.util.Map;
+
+public class Ex10 {
+  public static double averageStringLength(Stream<String> streamOfStrings) {
+    Map<Integer, Long> countOfStringsByLength = streamOfStrings
+    .collect(groupingBy(String::length, counting()));
+
+    long sum = 0;
+    long count = 0;
+    for (var entry : countOfStringsByLength.entrySet()) {
+      var length = entry.getKey();
+      var thisLengthCount = entry.getValue();
+      count += thisLengthCount;
+      sum += length * thisLengthCount;
+    }
+
+    if (count == 0) {
+      return 0.0;
+    }
+    return sum / (double) count;
+  }
+}
---
+++ src/test/java/core/java/se9/ch08/Ex10Test.java
@@ -0,0 +1,38 @@
+package core.java.se9.ch08;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Ex10Test {
+  @Test
+  void testAverageStringLengthWithManyStrings() {
+    var actual = Ex10.averageStringLength(
+      Stream.of(
+        "carbon",
+        "proves",
+        "dressing",
+        "hospital",
+        "cable",
+        "kidney",
+        "bolt"
+      )
+    );
+    double expected = 6.142857142857143;
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  void testAverageStringLengthWithNoStrings() {
+    var actual = Ex10.averageStringLength(Stream.empty());
+    assertEquals(0, actual);
+  }
+
+  @Test
+  void testAverageStringLengthWithOneString() {
+    var actual = Ex10.averageStringLength(Stream.of("honesty"));
+    assertEquals(7, actual);
+  }
+}
