a09c8022cafa5b1532753e4a772aac213aaeba4e
Darko Trifunovski <dtrifuno@gmail.com>
2020-09-09 13:29:01 +0000

    Solves Exercise 8.15

---
+++ src/main/java/core/java/se9/ch08/Ex15.java
@@ -0,0 +1,26 @@
+package core.java.se9.ch08;
+
+import java.util.stream.Stream;
+
+public class Ex15 {
+  public static double average(Stream<Double> stream) {
+    class Pair {
+      public final double sum;
+      public final int count;
+
+      public Pair(double sum, int count) {
+        this.count = count;
+        this.sum = sum;
+      }
+    }
+
+    Pair p = stream.reduce(
+      new Pair(0.0, 0),
+      (Pair pr, Double val) -> new Pair(pr.sum + val, pr.count + 1),
+      (Pair a, Pair b) -> new Pair(a.sum + b.sum, a.count + b.count));
+    if (p.count == 0) {
+      return 0;
+    }
+    return p.sum / p.count;
+  }
+}
---
+++ src/test/java/core/java/se9/ch08/Ex15Test.java
@@ -0,0 +1,28 @@
+package core.java.se9.ch08;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Ex15Test {
+  @Test
+  void testAverageWithManyEntries() {
+    Stream<Double> stream = Stream.of(2.4, 3.5, 1.1, 7.0, 5.0, 9.0);
+    var actual = Ex15.average(stream);
+    assertEquals(28 / 6.0, actual);
+  }
+
+  @Test
+  void testAverageWithOneEntry() {
+    var actual = Ex15.average(Stream.of(3.4));
+    assertEquals(3.4, actual);
+  }
+
+  @Test
+  void testAverageWithNoEntries() {
+    var actual = Ex15.average(Stream.empty());
+    assertEquals(0.0, actual);
+  }
+}
