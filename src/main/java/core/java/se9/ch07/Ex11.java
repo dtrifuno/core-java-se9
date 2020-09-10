3a0c0ceaddde89ab32c0a45758d99e94e09ec2e8
Darko Trifunovski <dtrifuno@gmail.com>
2020-09-10 01:57:23 +0000

    Solves Exercise 7.11

---
+++ src/main/java/core/java/se9/ch07/Ex11.java
@@ -0,0 +1,19 @@
+package core.java.se9.ch07;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex11 {
+  public static void main(String[] args) {
+    System.out.println("Please enter a sentence:");
+    Scanner in = new Scanner(System.in);
+    String sentence = in.nextLine();
+
+    ArrayList<String> words = new ArrayList<>(List.of(sentence.split("\\PL+")));
+    Collections.shuffle(words.subList(1, words.size()-1));
+    System.out.println(String.join(" ", words));
+    in.close();
+  }
+}
