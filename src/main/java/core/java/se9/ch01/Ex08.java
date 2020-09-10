9f7b69941e86d4a1256619c32170c07846a0344e
Darko Trifunovski <dtrifuno@gmail.com>
2020-09-10 16:25:28 +0000

    Solves Exercise 1.8

---
+++ src/main/java/core/java/se9/ch01/Ex08.java
@@ -0,0 +1,24 @@
+package core.java.se9.ch01;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Ex08 {
+  public static void main(String[] args) {
+    System.out.println("Please enter a string: ");
+    String input;
+    try (Scanner in = new Scanner(System.in)) {
+      input = in.nextLine();
+    }
+
+    Set<String> substrings = new HashSet<>();
+    for (int i = 0; i < input.length(); i++) {
+      for (int j = i + 1; j < input.length(); j++) {
+        substrings.add(input.substring(i, j));
+      }
+    }
+    System.out.println(substrings);
+  }
+  
+}
